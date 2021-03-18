0f86e13bd3790614f2dc9e64529c77424619bad0
bob2204 <78809253+bob2204@users.noreply.github.com>
2021-03-18 17:52:07 +0100

    Update init.sql

--- scripts/init.sql
+++ scripts/init.sql
@@ -7,8 +7,8 @@ prenom VARCHAR(20) NOT NULL,
 nom    VARCHAR(30) NOT NULL,
 email  VARCHAR(50) NOT NULL,
 UNIQUE (prenom,nom));
-INSERT INTO stage.stagiaires (prenom','nom','email) VALUES ('Émilie','Parker','eparker@gmail.com');
-INSERT INTO stage.stagiaires (prenom','nom','email) VALUES ('Igor','Gonzola','igonzola@hotmail.com');
-INSERT INTO stage.stagiaires (prenom','nom','email) VALUES ('Amapola','Perez','aperez@gmail.com');
-INSERT INTO stage.stagiaires (prenom','nom','email) VALUES ('Aymar','Tine','atine@gmail.com');
-INSERT INTO stage.stagiaires (prenom','nom','email) VALUES ('Rosalie','Constant','rconstant@gmail.com');
+INSERT INTO stage.stagiaires (prenom,nom,email) VALUES ('Émilie','Parker','eparker@gmail.com');
+INSERT INTO stage.stagiaires (prenom,nom,email) VALUES ('Igor','Gonzola','igonzola@hotmail.com');
+INSERT INTO stage.stagiaires (prenom,nom,email) VALUES ('Amapola','Perez','aperez@gmail.com');
+INSERT INTO stage.stagiaires (prenom,nom,email) VALUES ('Aymar','Tine','atine@gmail.com');
+INSERT INTO stage.stagiaires (prenom,nom,email) VALUES ('Rosalie','Constant','rconstant@gmail.com');
