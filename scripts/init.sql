bc8542421302581fac58b554d452fd3b51900ccf
Bruno Guérin <bguerin@dawan.fr>
2021-03-19 16:46:22 +0100

    [FIX] init.sql

--- scripts/init.sql
+++ scripts/init.sql
@@ -2,11 +2,11 @@ CREATE DATABASE stage;
 CREATE USER <USERNAME>@<IP_DOCKER_1> IDENTIFIED BY <PASSWORD>;
 GRANT ALL PRIVILEGES ON stage.* TO <USERNAME>;
 CREATE TABLE stage.stagiaires (
-id     INT AUTO_INCREMENT PRIMARY_KEY,
+id     INT AUTO_INCREMENT PRIMARY KEY,
 prenom VARCHAR(20) NOT NULL,
 nom    VARCHAR(30) NOT NULL,
 email  VARCHAR(50) NOT NULL,
-UNIQUE (prenom,nom));
+UNIQUE(prenom,nom));
 INSERT INTO stage.stagiaires (prenom,nom,email) VALUES ('Émilie','Parker','eparker@gmail.com');
 INSERT INTO stage.stagiaires (prenom,nom,email) VALUES ('Igor','Gonzola','igonzola@hotmail.com');
 INSERT INTO stage.stagiaires (prenom,nom,email) VALUES ('Amapola','Perez','aperez@gmail.com');
